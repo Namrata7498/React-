d46710466d3f89d6bc7cd99cee3d23c1222d7396
Namrata7498 <mnamrata578@gmail.com>
2024-04-15 12:58:30 +0530

    test file created

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -16,17 +16,6 @@ function Navbar(props){
         <li className="nav-item">
           <a className="nav-link" href="#">{props.abttext}</a>
         </li>
-        <li className="nav-item dropdown">
-          <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Dropdown
-          </a>
-          <ul className="dropdown-menu">
-            <li><a className="dropdown-item" href="#">Action</a></li>
-            <li><a className="dropdown-item" href="#">Another action</a></li>
-            <li><hr className="dropdown-divider"/></li>
-            <li><a className="dropdown-item" href="#">Something else here</a></li>
-          </ul>
-        </li>
         <li className="nav-item">
           <a className="nav-link disabled" aria-disabled="true">Disabled</a>
         </li>
